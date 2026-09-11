61c5c6638511ed36266aede6f56d862374c05eb5
catomat0 <catomat0@github.com>
2026-09-11 11:38:33 +0900

    chore: 유저 이메일 유니크 제약 해제

--- src/main/java/com/atmd/backend/domain/user/entity/User.java
+++ src/main/java/com/atmd/backend/domain/user/entity/User.java
@@ -23,7 +23,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     // @Column
