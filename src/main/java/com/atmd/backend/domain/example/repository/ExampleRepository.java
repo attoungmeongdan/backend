81460131ac8b03dc4d9c38494ad1c9de826c939b
rkdehdrbs7885-oss <rkdehdrbs7885@gmail.com>
2026-09-11 14:59:39 +0900

    chore: 미사용 파일 삭제 (#60)

--- src/main/java/com/atmd/backend/domain/example/repository/ExampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.atmd.backend.domain.example.repository;
-
-import com.atmd.backend.domain.example.entity.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleRepository extends JpaRepository<Example, Long> {
-}
